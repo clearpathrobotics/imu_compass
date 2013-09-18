dec30b92f1c0cdbd1ab06bb428c7ccaec6670e9c
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2013-09-18 13:11:14 -0400

    license files added

--- src/imu_compass.cpp
+++ src/imu_compass.cpp
@@ -1,3 +1,28 @@
+/*
+imu_compass.cpp
+Authors: Prasenjit (pmukherj@clearpathrobotics.com)
+Copyright (c) 2013, Clearpath Robotics, Inc., All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Description:
+CPP file for IMU Compass Class that combines gyroscope and magnetometer data to get a clean estimate of yaw. 
+*/
+
+
 #include "imu_compass/imu_compass.h"
 
 IMUCompass::IMUCompass(ros::NodeHandle &n):node_(n) 
@@ -9,7 +34,7 @@ IMUCompass::IMUCompass(ros::NodeHandle &n):node_(n)
 	node_.getParam("mag_bias/x",mag_zero_x_); 
 	node_.getParam("mag_bias/y",mag_zero_y_);
 	node_.getParam("mag_bias/z",mag_zero_z_);	
-  ROS_INFO("Using magnetometer bias (x,y):%f,%f",mag_zero_x_, mag_zero_y_);
+	ROS_INFO("Using magnetometer bias (x,y):%f,%f",mag_zero_x_, mag_zero_y_);
 
 	sensor_timeout_ = 0.5;
 	yaw_meas_variance_ = 0.5; //TODO: Make this tunable as a parameter, not a priority
